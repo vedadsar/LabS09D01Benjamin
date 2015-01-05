c9233d63cbc5c2d165fa315e5ecd1a44590a0786
vedadsar <vedadsa@windowslive.com>
2015-01-05 19:12:06 +0100

    Edited GUI app

--- GUIBinaryGame.java
+++ GUIBinaryGame.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +10,7 @@ import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -19,6 +21,7 @@ public class GUIBinaryGame extends JFrame {
 	static JButton correct;
 	static JButton higher;
 	static JTextField text;
+	static JLabel textCounter;
 	static int [] array = new int [10000];
 	static int startPosition;
 	static int endPosition;
@@ -45,17 +48,24 @@ public class GUIBinaryGame extends JFrame {
 	lower = new JButton("Lower");			//Lower dugme, klikamo ukoliko je nas broj manji od pokusaja racunara
 	 correct = new JButton("Correct");		// Ukoliko je racunar pogodio nas broj
 	 higher = new JButton("Higher");		//Higher dugme klikamo ukoliko je nas broj veci od pokusjaa racunara
+	textCounter = new JLabel("Broj pokusaja " +counter);
+	textCounter.setFont(new Font("Arial", Font.ITALIC, 22));
+	
 	text = new JTextField("Da li je vas broj " +middleIndex +"? ");		//Postavljamo text 
 	text.setEnabled(false);					//Stavljamo da se tekst ne moze editovati od korisnika
 	text.setHorizontalAlignment(JTextField.CENTER);
+	text.setFont(new Font("Arial", Font.BOLD, 28));
 	
-	window.setLayout(new BorderLayout());	
-	window.add(text, BorderLayout.NORTH);
-	window.add(lower, BorderLayout.WEST);
-	window.add(higher,BorderLayout.EAST);
-	window.add(correct,BorderLayout.CENTER);
-	window.setSize(500, 150);
+	window.setLayout(new FlowLayout());	
+	window.add(text);
+	window.add(lower);
+	window.add(higher);
+	window.add(correct);
+	window.add(textCounter);
+	window.setSize(350, 150);
 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.setResizable(false);
+	window.setLocation(500, 500);
 	window.setVisible(true);
 
 	
@@ -82,7 +92,8 @@ public class GUIBinaryGame extends JFrame {
 				counter++;
 				endPosition = middleIndex - 1;
 				middleIndex = (startPosition + endPosition) / 2;
-			    text.setText("Da li je vas broj " +middleIndex +"? A broj pokusaja je  " +counter);
+			    text.setText("Da li je vas broj " +middleIndex +"?" );
+			    textCounter.setText("Broj pokusaja " +counter);
 									
 			}
 			//Uslov ukoliko je kliknuto dugme higher
@@ -90,7 +101,8 @@ public class GUIBinaryGame extends JFrame {
 				counter++;
 				startPosition = middleIndex + 1;
 				middleIndex = (startPosition + endPosition) / 2;
-			    text.setText("Da li je vas broj " +middleIndex +"? A broj pokusaja je "+counter);
+			    text.setText("Da li je vas broj " +middleIndex +"? ");
+			    textCounter.setText("Broj pokusaja " +counter);
 									
 			}
 			//Ukoliko je kliknuto dugme correct.
